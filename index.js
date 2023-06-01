b744b0bffd2e4591b1efac70a83459510d9187b3
Arf <51786641+myaltithink@users.noreply.github.com>
2023-06-01 19:24:40 +0800

    done

--- index.js
+++ index.js
@@ -1,43 +1,149 @@
 const bitValues = [128, 64, 32, 16, 8, 4, 2, 1];
 const bitValuesIndex = bitValues.length;
 
-const ip = "192.168.10.0";
-const hostCount = 280;
 let usableHosts = "";
 
 let subnetMask = "0.0.0.0";
-let netMask = 1;
+let netMask = 0;
 let subnetClassCount = 0;
 
 let bitCounts = [0, 0, 0, 0];
 let remainingBits = 0;
 let computedRemainingBits = 0;
 
-findClassBitBasedOnHost(hostCount);
-findClassBit(netMask);
-getSubnetMask();
+let numberOfNetworks = 0;
+let ip = [0, 0, 0, 0];
 
-function findClassBitBasedOnHost(host){
+let answersContainer = null;
+let answer = "";
+
+
+window.addEventListener("load", () => {
+    answersContainer = document.getElementById("answers")
+    document.getElementById("start-calculation").addEventListener("click", () => {
+        const octets = document.getElementsByClassName("ip");
+        const netmask = document.getElementById("netmask");
+        const non = document.getElementById("networks");
+        const host = document.getElementById("hosts");
+        const empty = /^\s*$/;
+
+        let missingip = false;
+
+        for (const octet of octets) {
+            let name = octet.name.split("-");
+            let index = Number(name[1]) - 1;
+
+            if (empty.test(octet.value)) {
+                missingip = true;
+                break;
+            }
+
+            ip[index] = Number(octet.value);
+        }
+
+        if (empty.test(netmask.value) || empty.test(non.value) || missingip) {
+            answer = "Missing field has been detected"
+            answersContainer.innerText = answer;
+            clear()
+            return;
+        }
+
+        netMask = netmask.value;
+        numberOfNetworks = non.value
+
+        if (!empty.test(host.value)) {
+            findClassBitBasedOnHost(host.value);
+        }
+        findClassBit();
+        getSubnetMask();
+        getNetworkMinMax();
+        answersContainer.innerText = answer;
+        clear();
+    });
+
+    document.getElementById("find-netmask").addEventListener("click", () => {
+        const host = document.getElementById("hosts");
+        let hosts = host.value;
+        findClassBitBasedOnHost(hosts);
+        answersContainer.innerText = answer
+        document.getElementById("netmask").value = netMask
+        clear();
+    });
+})
+
+function clear() {
+    usableHosts = "";
+    subnetMask = "0.0.0.0";
+    netMask = 0;
+    subnetClassCount = 0;
+    bitCounts = [0, 0, 0, 0];
+    remainingBits = 0;
+    computedRemainingBits = 0;
+    numberOfNetworks = 0;
+    ip = [0, 0, 0, 0];
+    answer = "";
+}
+
+function getNetworkMinMax() {
+    let networks = computedRemainingBits;
+    for (let i = 0; i < numberOfNetworks; i++) {
+        answer = answer + `\nSub ${i}: ${displayIp()}\n`;
+        ip[3]++;
+        networks--;
+        checkIp();
+
+        answer = answer + `1st: ${displayIp()}\n`;
+        for (let j = 0; j < networks - 2; j++) {
+            ip[3]++;
+            checkIp();
+        }
+        networks -= ip[3];
+
+        answer = answer + `last: ${displayIp()}\n`;
+
+        ip[3]++;
+        checkIp()
+        networks = computedRemainingBits;
+        answer = answer + `broadcast: ${displayIp()}\n`;
+        ip[3]++;
+        checkIp();
+    }
+}
+
+function checkIp() {
+    for (let i = 0; i < ip.length; i++) {
+        if (ip[i] >= 256) {
+            let prevOctet = i - 1;
+            ip[i] = 0;
+            ip[prevOctet]++;
+        }
+    }
+}
+
+function displayIp() {
+    return ` ${ip[0]}.${ip[1]}.${ip[2]}.${ip[3]}`;
+}
+
+function findClassBitBasedOnHost(host) {
     netMask = 32;
     findClassBit(netMask);
-    while(computedRemainingBits - 2 < host){
+    while (computedRemainingBits < host) {
         console.log("bits: " + netMask + " | hosts: " + computedRemainingBits + " | usable hosts: " + (computedRemainingBits - 2));
         console.log()
         netMask--;
         remainingBits = 0;
         findClassBit(netMask);
     }
-    console.log(`Suitable Bits for ${host} hosts is ` + netMask + " bits");
+    answer = `Suitable Bits for ${host} hosts is ` + netMask + " bits";
     usableHosts = `Hosts: ${computedRemainingBits} - 2 = ${computedRemainingBits - 2} usable hosts`;
-    console.log(usableHosts);
-    console.log()
-    
+    answer = answer + "\n" + usableHosts;
+    console.log(netMask)
     remainingBits = 0;
     computedRemainingBits = 0;
-    
+
 }
 
-function findClassBit(netMask){
+function findClassBit() {
     let totalCount = 0;
     let currentBitIndex = 0;
     let currentBit = 0;
@@ -68,7 +174,6 @@ function findClassBit(netMask){
     computedRemainingBits = Math.pow(2, remainingBits);
 }
 
-
 function getSubnetMask() {
     //get subnet mask values
     let subnetValues = [0, 0, 0, 0];
@@ -81,7 +186,7 @@ function getSubnetMask() {
             case 0:
                 subnetClassCount[i] = 0;
                 break;
-        
+
             default:
                 for (let j = 0; j < bitCounts[i]; j++) {
                     subnetValues[i] += bitValues[j];
@@ -89,10 +194,10 @@ function getSubnetMask() {
                 break;
         }
     }
-    console.log("bits: " + bitCounts)
-    console.log("remaining: " + remainingBits)
+    answer = answer + "\nbits: " + bitCounts + "\n";
+    answer = answer + "remaining: " + remainingBits + "\n";
     subnetMask = `${subnetValues[0]}.${subnetValues[1]}.${subnetValues[2]}.${subnetValues[3]}`;
     usableHosts = `${computedRemainingBits} - 2 = ${computedRemainingBits - 2} usable hosts`;
-    console.log("Subnet Mask: " + subnetMask)
-    console.log("Hosts: " + usableHosts)
+    answer = answer + "Subnet Mask: " + subnetMask + "\n";
+    answer = answer + "Hosts: " + usableHosts + "\n";
 }
